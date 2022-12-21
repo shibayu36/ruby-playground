a6f2492f43aeb2b93a1b86228710129b8b409b64
shibayu36 <shibayu36@gmail.com>
2022-12-21 14:07:37 +0900

    Implement #parse with dirty code

--- lib/ltsv/parser.rb
+++ lib/ltsv/parser.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
+require 'ltsv/log'
+
 module Ltsv
   class Parser
     def initialize(filename:)
@@ -5,8 +9,13 @@ module Ltsv
     end
 
     def parse
-      File.foreach(@filename) do |line|
-        p(line)
+      File.foreach(@filename).map do |line|
+        fields = {}
+        line.chomp.split("\t").each do |field|
+          key_val = field.split(':', 2)
+          fields[key_val[0].to_sym] = key_val[1] == '-' ? nil : key_val[1]
+        end
+        Ltsv::Log.new(**fields)
       end
     end
   end
--- spec/ltsv/parser_spec.rb
+++ spec/ltsv/parser_spec.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'ltsv/log'
 require 'ltsv/parser'
 
 describe Ltsv::Parser do
@@ -12,9 +13,33 @@ describe Ltsv::Parser do
   end
 
   describe '#parse' do
-    it do
+    it 'returns parsed array' do
       parser = Ltsv::Parser.new(filename: File.expand_path('../fixtures/files/1.ltsv', File.dirname(__FILE__)))
-      parser.parse
+      result = parser.parse
+
+      expect(result[0].host).to eq '127.0.0.1'
+      expect(result[0].user).to eq 'frank'
+      expect(result[0].epoch).to eq 1_372_694_390
+      expect(result[0].req).to eq 'GET /apache_pb.gif HTTP/1.0'
+      expect(result[0].status).to eq 200
+      expect(result[0].size).to eq 2326
+      expect(result[0].referer).to eq 'http://www.hatena.ne.jp/'
+
+      expect(result[1].host).to eq '127.0.0.1'
+      expect(result[1].user).to eq 'john'
+      expect(result[1].epoch).to eq 1_372_794_390
+      expect(result[1].req).to eq 'GET /apache_pb.gif HTTP/1.0'
+      expect(result[1].status).to eq 200
+      expect(result[1].size).to eq 1234
+      expect(result[1].referer).to eq 'http://b.hatena.ne.jp/hotentry'
+
+      expect(result[2].host).to eq '127.0.0.1'
+      expect(result[2].user).to eq nil
+      expect(result[2].epoch).to eq 1_372_894_390
+      expect(result[2].req).to eq 'GET /apache_pb.gif HTTP/1.0'
+      expect(result[2].status).to eq 302
+      expect(result[2].size).to eq 9999
+      expect(result[2].referer).to eq 'http://www.example.com/start.html'
     end
   end
 end
