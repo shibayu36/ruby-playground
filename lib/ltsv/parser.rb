13fff9939565235a96f04774c399d40cdab36158
shibayu36 <shibayu36@gmail.com>
2022-12-23 13:12:49 +0900

    Refactor #parse

--- lib/ltsv/parser.rb
+++ lib/ltsv/parser.rb
@@ -10,12 +10,11 @@ module Ltsv
 
     def parse
       File.foreach(@filename).map do |line|
-        fields = {}
-        line.chomp.split("\t").each do |field|
+        fields = line.chomp.split("\t").map do |field|
           key_val = field.split(':', 2)
-          fields[key_val[0].to_sym] = key_val[1] == '-' ? nil : key_val[1]
+          [key_val[0].to_sym, key_val[1] == '-' ? nil : key_val[1]]
         end
-        Ltsv::Log.new(**fields)
+        Ltsv::Log.new(**fields.to_h)
       end
     end
   end
