b149b07deea8875f1d3042413e8e44335c30e97d
shibayu36 <shibayu36@gmail.com>
2022-12-23 13:33:34 +0900

    Implement Ltsv::Log2 to support variable length fields unlike Ltsv::Log

---
+++ lib/ltsv/log2.rb
@@ -0,0 +1,33 @@
+module Ltsv
+  class Log2
+    def initialize(**fields)
+      fields.each do |key, value|
+        instance_variable_set("@#{key}", value)
+      end
+    end
+
+    def method
+      @req ? @req.split(' ')[0] : nil
+    end
+
+    def path
+      @req ? @req.split(' ')[1] : nil
+    end
+
+    def protocol
+      @req ? @req.split(' ')[2] : nil
+    end
+
+    def uri
+      return unless @host
+
+      "http://#{@host}#{path}"
+    end
+
+    def time
+      return unless @epoch
+
+      Time.at(@epoch.to_i).utc.strftime('%Y-%m-%dT%H:%M:%S')
+    end
+  end
+end
---
+++ spec/ltsv/log2_spec.rb
@@ -0,0 +1,103 @@
+# frozen_string_literal: true
+
+require 'ltsv/log2'
+
+describe Ltsv::Log2 do
+  let(:req) { 'GET /apache_pb.gif HTTP/1.0' }
+  let(:host) { '127.0.0.1' }
+  let(:epoch) { '1372694390' }
+  let(:log) do
+    Ltsv::Log2.new(
+      host: host,
+      user: 'frank',
+      epoch: epoch,
+      req: req,
+      status: '200',
+      size: '2326',
+      referer: 'http://www.hatena.ne.jp/'
+    )
+  end
+
+  describe '#method' do
+    context 'when req is defined' do
+      it 'returns method name in req' do
+        expect(log.method).to eq 'GET'
+      end
+    end
+
+    context 'when req is undefined' do
+      let(:req) { nil }
+      it 'returns nil' do
+        expect(log.method).to eq nil
+      end
+    end
+  end
+
+  describe '#path' do
+    context 'when req is defined' do
+      it 'returns path in req' do
+        expect(log.path).to eq '/apache_pb.gif'
+      end
+    end
+
+    context 'when req is undefined' do
+      let(:req) { nil }
+      it 'returns nil' do
+        expect(log.path).to eq nil
+      end
+    end
+  end
+
+  describe '#protocol' do
+    context 'when req is defined' do
+      it 'returns protocol in req' do
+        expect(log.protocol).to eq 'HTTP/1.0'
+      end
+    end
+
+    context 'when req is undefined' do
+      let(:req) { nil }
+      it 'returns nil' do
+        expect(log.protocol).to eq nil
+      end
+    end
+  end
+
+  describe '#uri' do
+    context 'when host is undefined' do
+      let(:host) { nil }
+      it 'returns nil' do
+        expect(log.uri).to eq nil
+      end
+    end
+
+    context 'when req is undefined' do
+      let(:req) { nil }
+
+      it 'returns uri without path' do
+        expect(log.uri).to eq 'http://127.0.0.1'
+      end
+    end
+
+    context 'when both host and path are defined' do
+      it 'returns uri' do
+        expect(log.uri).to eq 'http://127.0.0.1/apache_pb.gif'
+      end
+    end
+  end
+
+  describe '#time' do
+    context 'when epoch is undefined' do
+      let(:epoch) { nil }
+      it 'returns nil' do
+        expect(log.time).to eq nil
+      end
+    end
+
+    context 'when epoch is defined' do
+      it 'returns formatted time' do
+        expect(log.time).to eq '2013-07-01T15:59:50'
+      end
+    end
+  end
+end
