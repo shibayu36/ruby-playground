8ff1cc44f2bc2295b29395aea9c70fcfef7f224d
shibayu36 <shibayu36@gmail.com>
2022-12-25 13:07:08 +0900

    Commonize the tempfile part

--- spec/ltsv/log_counter_spec.rb
+++ spec/ltsv/log_counter_spec.rb
@@ -5,35 +5,49 @@ require 'ltsv/parser'
 require 'ltsv/log_counter'
 
 describe Ltsv::LogCounter do
+  let(:ltsv) { '' }
+
+  def with_tempfile
+    Tempfile.create('ltsv') do |f|
+      f.puts(ltsv)
+      f.flush
+      yield f.path
+    end
+  end
+
   describe '#count_error' do
+    let(:ltsv) do
+      <<~EOS
+        host:127.0.0.1\tstatus:200
+        host:127.0.0.1\tstatus:401
+        host:127.0.0.1\tstatus:500
+        host:127.0.0.1\tstatus:302
+        host:127.0.0.1\tstatus:503
+      EOS
+    end
+
     it 'returns the number of logs with status greater than 500' do
-      Tempfile.create('ltsv') do |f|
-        f.puts <<~EOS
-          host:127.0.0.1\tstatus:200
-          host:127.0.0.1\tstatus:401
-          host:127.0.0.1\tstatus:500
-          host:127.0.0.1\tstatus:302
-          host:127.0.0.1\tstatus:503
-        EOS
-        f.flush
-        counter = Ltsv::LogCounter.new(Ltsv::Parser.new(filename: f.path).parse)
+      with_tempfile do |path|
+        counter = Ltsv::LogCounter.new(Ltsv::Parser.new(filename: path).parse)
         expect(counter.count_error).to eq 2
       end
     end
   end
 
   describe '#group_by_user' do
+    let(:ltsv) do
+      <<~EOS
+        user:john\tnumber:1
+        user:frank\tnumber:2
+        user:-\tnumber:3
+        user:john\tnumber:4
+        user:-\tnumber:5
+      EOS
+    end
+
     it 'returns hash grouping logs by user' do
-      Tempfile.create('ltsv') do |f|
-        f.puts <<~EOS
-          user:john\tnumber:1
-          user:frank\tnumber:2
-          user:-\tnumber:3
-          user:john\tnumber:4
-          user:-\tnumber:5
-        EOS
-        f.flush
-        counter = Ltsv::LogCounter.new(Ltsv::Parser.new(filename: f.path).parse)
+      with_tempfile do |path|
+        counter = Ltsv::LogCounter.new(Ltsv::Parser.new(filename: path).parse)
 
         res = counter.group_by_user
         expect(res['john'].map(&:number)).to eq %w[1 4]
