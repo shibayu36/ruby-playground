40c1d3a3c2ee6bbab57ded9d0dc560c37860b0d1
shibayu36 <shibayu36@gmail.com>
2022-12-25 12:52:23 +0900

    Implement group_by_user

--- lib/ltsv/log_counter.rb
+++ lib/ltsv/log_counter.rb
@@ -9,5 +9,15 @@ module Ltsv
     def count_error
       @logs.count { |log| log.status.to_i >= 500 }
     end
+
+    def group_by_user
+      res = {}
+      @logs.each do |log|
+        user = log.user || 'guest'
+        res[user] ||= []
+        res[user].push(log)
+      end
+      res
+    end
   end
 end
--- spec/ltsv/log_counter_spec.rb
+++ spec/ltsv/log_counter_spec.rb
@@ -21,4 +21,25 @@ describe Ltsv::LogCounter do
       end
     end
   end
+
+  describe '#group_by_user' do
+    it 'returns hash grouping logs by user' do
+      Tempfile.create('ltsv') do |f|
+        f.puts <<~EOS
+          user:john\tnumber:1
+          user:frank\tnumber:2
+          user:-\tnumber:3
+          user:john\tnumber:4
+          user:-\tnumber:5
+        EOS
+        f.flush
+        counter = Ltsv::LogCounter.new(Ltsv::Parser.new(filename: f.path).parse)
+
+        res = counter.group_by_user
+        expect(res['john'].map(&:number)).to eq %w[1 4]
+        expect(res['frank'].map(&:number)).to eq %w[2]
+        expect(res['guest'].map(&:number)).to eq %w[3 5]
+      end
+    end
+  end
 end
